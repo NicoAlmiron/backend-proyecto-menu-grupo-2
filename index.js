a16da45c06e320d7b72101da10cc0f76b7e35e8d
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-09-27 20:39:43 -0300

    Inicializando usuarios

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import 'dotenv/config';
-import menuRouter from "./src/routes/menus.routes.js";
-import './src/database/database.js';
+// import menuRouter from "./src/routes/menus.routes.js";
+// import './src/database/database.js';
 
-const app = express();
+// const app = express();
 
-app.set('port', process.env.PORT || 4000);
-app.listen(app.get('port'), () => {
-    console.log('puerto conectado: ' + app.get('port'))
-});
+// app.set('port', process.env.PORT || 4000);
+// app.listen(app.get('port'), () => {
+//     console.log('puerto conectado: ' + app.get('port'))
+// });
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(morgan('dev'));
 
-app.use('/api', menuRouter);
+// app.use('/api', menuRouter);
