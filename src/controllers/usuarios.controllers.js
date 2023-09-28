9afa6e4622727573e62b8d1011dbe99cb1f4d6fd
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-09-28 20:41:51 -0300

    Agregando usuarios de prueba a la base de datos. Todo OK

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -10,12 +10,21 @@ export const listarUsuarios = (req, res) => {
     }
 };
 
-export const crearUsuarios = (req, res) => {
+export const crearUsuarios = async (req, res) => {
     try{
-        console.log(req.body)
-        res.send('Prueba crearUsuarios')
+       
+        const usuarioNUevo = new Usuario(req.body)
+        
+        await usuarioNUevo.save();
+        res.status(201).json({
+            mensaje: "El usuario fue creado correctamente"
+        })
+
 
     }catch(error){
-        console.log(error)
+        console.log(error);
+        res.status(400).json({
+            mensaje: "El usuario no pudo ser creado"
+        })
     }
 }
