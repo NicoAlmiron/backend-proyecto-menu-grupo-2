4db8d30ae98eb3b66c1812b433d7e979be39caa7
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-09-29 21:09:21 -0300

    Agrego direccionamiento a un archivo estatico para el inicio en VERCEL

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ import morgan from "morgan";
 import 'dotenv/config';
 import usuarioRouter from "./src/routes/usuarios.routes.js";
 import './src/database/database.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 const app = express();
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), () => {
     console.log('puerto conectado: ' + app.get('port'))
 });
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
+app.use(express.static(path.join(_dirname, '/public')));
 app.use('/api/users', usuarioRouter);
