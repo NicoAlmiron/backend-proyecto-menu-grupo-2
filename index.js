c3a0d3059b89930a9c77b84ff9c7c7de11cbedb8
Nico Almiron <almiron.nicolas16@gmail.com>
2023-10-01 05:05:42 -0300

    se corrijieron errores

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import 'dotenv/config';
 import usuarioRouter from "./src/routes/usuarios.routes.js";
 import menuRouter from "./src/routes/menus.routes.js";
+import pedidosRouter from "./src/routes/pedidos.routes.js"
 import './src/database/database.js'
 
 
@@ -19,4 +20,3 @@ app.use(morgan('dev'));
 app.use('/api/users', usuarioRouter);
 app.use('/api', menuRouter);
 app.use("/api", pedidosRouter);
-
--- src/models/pedidos.js
+++ src/models/pedidos.js
@@ -1,22 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
 const pedidosSchema = new Schema({
-  usuario: { type: String, required: true },
-  fecha: {
-    type: String,
-    required: true,
-    match:
-      /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4} (0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/,
-  },
-  menu: {
-    type: Array,
-    required: true,
-  },
-  estado: {
-    type: Boolean,
-    required: true,
-  },
-});
-const Pedidos = mongoose.model("pedido", pedidosSchema);
+    usuario: { type: String, required: true },
+    fecha: {
+        type: String,
+        required: true,
 
-export default Pedidos;
+    },
+    menu: {
+        type: Array,
+        required: true,
+    },
+    estado: {
+        type: Boolean,
+        required: true,
+    },
+});
+const Pedido = mongoose.model("pedido", pedidosSchema);
+
+export default Pedido;
